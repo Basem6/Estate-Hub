109bb345efaec60ead3ad6c7acb169378d3188f9
Basem Mahmoud <basem556473@gmail.com>
2026-09-21 06:23:21 +0300

    esd

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    // Cache optimized images for 1 year
-    minimumCacheTTL: 31536000,
-  },
-  // Enable compression
-  compress: true,
-  // Optimize production builds
-  swcMinify: true,
-};
+import type { NextConfig } from "next";
 
-module.exports = nextConfig;
+const nextConfig: NextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
+export default nextConfig;
